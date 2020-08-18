65959405620926cf95942b7cf4e8124d83b15b6b
im pro <69852783+muujig0108@users.noreply.github.com>
2020-08-19 03:15:55 +0800

    commit

---
+++ img.js
@@ -0,0 +1,19 @@
+[
+    {
+        "ID": "001",
+        "Name": "Angular",
+        "Image": "img/dark.jpg"
+    },
+
+    {
+        "ID": "002",
+        "Name": "JSON",
+        "Image": "img/logo.png"
+    },
+
+    {
+        "ID": "003",
+        "Name": "shaw.Net",
+        "Image": "img/shaw.jpg"
+    }
+]
